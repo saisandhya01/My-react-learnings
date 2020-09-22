9e9d2ae940045cd391ac0f45cd6af339a6281cf4
Sai <saisandhya187@gmail.com>
2020-09-22 17:42:12 +0530

    removed access key

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID J8UnVCQ8hYCZOv9WqOvQ2AFSFPUWVX7lWUl6XRDEX2Y",
+    Authorization: "Client-ID access_key",
   },
 });
